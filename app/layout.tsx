2dec55705ca1b5c99261ba554420e73b588298bd
Igor Duca <yelldutz@gmail.com>
2024-08-03 22:43:15 -0300

    feat: opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,19 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Eemail",
   description: "This design is a copy of a design created by dave (@heyXdave).",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://eemail.vercel.app/",
+    images: [
+      {
+        url: "/thumb.jpeg",
+        width: 1200,
+        height: 630,
+        alt: "Eemail",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
